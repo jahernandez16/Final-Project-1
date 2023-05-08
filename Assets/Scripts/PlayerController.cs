c846c4449acf77dd61dcb3af4a8e4c260b550c48
D0m352 <122396683+D0m352@users.noreply.github.com>
2023-05-08 11:42:53 -0700

    player controller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    public float Speed = 5.0f;
+    public bool hasPowerup = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>(); 
+        //focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * Speed);
     }
+     private void OnTriggerEnter(Collider other)
+     {
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;   
+            Destroy(other.gameObject); 
+        }
+     }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Collied with: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+        }
+    }
+
 }
